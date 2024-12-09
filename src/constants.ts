f39dda5b610aa39953d8a69187912aa6be68ef74
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-12-09 22:24:04 +0300

    prod

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'production' ? import.meta.env.VITE_PROD_URL : import.meta.env.VITE_BASE_URL
+  process.env.NODE_ENV === 'production'
+    ? process.env.VITE_PROD_URL
+    : import.meta.env.VITE_BASE_URL
