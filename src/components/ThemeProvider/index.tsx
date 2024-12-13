219682699db926823303be66b8ad02b046744993
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-12-13 22:14:23 +0300

    fix toats

--- src/components/ThemeProvider/index.tsx
+++ src/components/ThemeProvider/index.tsx
@@ -1,11 +1,13 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { Toaster } from 'react-hot-toast'
 
 function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="system">
         <main className="min-h-screen">{children}</main>
+        <Toaster />
       </NextThemesProvider>
     </NextUIProvider>
   )
