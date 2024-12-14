aae268233a3eeeef0ff2afe105b767e2dcb5fe52
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-12-14 18:45:05 +0300

    fix link

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://social-net-back.onrender.com/'
+    ? 'https://social-net-back.onrender.com'
     : import.meta.env.VITE_BASE_URL
