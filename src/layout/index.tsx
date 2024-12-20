fc95fcf054b586aa5e9ecd952fae7839703f3ab9
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-12-21 01:13:40 +0300

    fix layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,19 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import Container from '../components/shared/Container'
 import Header from '../components/shared/Header'
 import NavBar from '../components/shared/NavBar'
 import MobileNavBar from '../components/shared/MobileNavBar'
 import { useSelector } from 'react-redux'
-import { selectIsAuthenticated, selectUser } from '../features/user/user.slice'
+import { selectIsAuthenticated } from '../features/user/user.slice'
 import { useEffect } from 'react'
 import Profile from '../components/shared/Profile'
 
 function Layout() {
   const isAuth = useSelector(selectIsAuthenticated)
-  const user = useSelector(selectUser)
+  const location = useLocation()
   const navigate = useNavigate()
+
+  const isUserProfilePage = location.pathname.includes('/users/')
 
   useEffect(() => {
     if (!isAuth) {
@@ -29,7 +31,7 @@ function Layout() {
         <div className="flex-2 p-4 overflow-auto flex-grow pb-20 lg:pb-4">
           <Outlet />
         </div>
-        {!user && (
+        {!isUserProfilePage && (
           <div className="flex-2 p-4 lg:sticky lg:top-16 hidden lg:block">
             <div className="flex-col flex gap-5">
               <Profile />
