00af64fe6f85ec20b43d71c61adebbcdc8d49a5f
Leonid-Korobkov <korobkov-leo@mail.ru>
2025-05-04 22:38:02 +0300

    fix: обратно вернул middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+  const token = request.cookies.get('token')?.value
+  const isAuthPage = request.nextUrl.pathname.startsWith('/auth')
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL('/auth', request.url))
+  }
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL(request.nextUrl.pathname, request.url))
+  }
+
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // Нужно выбрать все страницы которые находятся в protected папке
+  matcher: [
+    // Защищенные страницы
+    '/search',
+    '/users/:path*',
+    '/posts/:path*',
+    '/',
+    '/auth',
+  ],
+}
