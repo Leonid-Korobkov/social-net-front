803c1595adda6b2f8a94631d8361a14f412907f6
Leonid-Korobkov <korobkov-leo@mail.ru>
2025-05-15 22:07:18 +0300

    fix: убран лишний класс темы для body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="Zling" />
       </head>
-      <body className={`antialiased green-dark`}>
+      <body className={`antialiased`}>
         <ThemeProvider>
           <StoreProvider>
             <ProtectedRoute>
